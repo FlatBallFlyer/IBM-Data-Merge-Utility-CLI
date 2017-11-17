778c74a13f82a2e615e4067f401af579789e3eb3
Mike Storey <mike@ssgpp.com>
2017-11-17 13:57:30 -0500

    Project Setup in Eclipse

---
+++ src/com/ibm/util/merge/cli/Merge.java
@@ -0,0 +1,14 @@
+package com.ibm.util.merge.cli;
+
+public class Merge {
+
+	public Merge() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
