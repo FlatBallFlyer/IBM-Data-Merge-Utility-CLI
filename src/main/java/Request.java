f97aaf0253e730e553fd637466c835b2c077a44f
Mike Storey <mike@ssgpp.com>
2017-12-02 20:48:36 -0500

    Maven fixes

--- src/main/java/Request.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.util.merge.cli;
-
-import java.util.HashMap;
-
-public class Request {
-	public HashMap<String, String[]> parameters;
-	public String payload;
-	public String outputFile;
-}
